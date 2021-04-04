da218512f6500639188c783d7a2905cb20e226f4
sondosalfalet <81251228+sondosalfalet@users.noreply.github.com>
2021-04-04 12:46:07 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('about', function () {
+    $name = 'Sondos' ;
+    return view('about' , compact('name'));
+});
+Route::post('send', function ( Request $request) {
+    $name = $request->myname ;
+    return view('about' , compact('name'));
 });
