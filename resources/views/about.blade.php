02031184e9467a4876f95d9b150901cf29d149df
sondosalfalet <81251228+sondosalfalet@users.noreply.github.com>
2021-04-04 12:47:24 +0300

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,1 +1,17 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1> My Name is {{$name}}</h1>
+    <form action="send" method="POST" >
+        @csrf
+        Name
+        <input type="text" name="myname" id="myname">
+        <input type="submit" value="send">
+</body>
+</html>
